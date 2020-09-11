033de4ab2f23a86b1dced34661ce782f60c6e564
cswil <cswilson@zoho.com>
2020-09-11 20:30:51 +0100

    Cookies and shit added

---
+++ models/db_models.py
@@ -0,0 +1,26 @@
+from db.base import Base
+from sqlalchemy import Boolean, Column, Integer, String, Float
+from sqlalchemy.types import DateTime
+from datetime import datetime
+from sqlalchemy.orm import relationship
+from random import randint
+class Recipe(Base):
+    __tablename__ = "recipes"
+    id = Column(Integer, primary_key=True, index=True)
+    rid = Column(Integer)
+    created_by = Column(String)
+    title = Column(String)
+    date_created = Column(DateTime, default=datetime.utcnow())
+    calories = Column(Integer, default=0)
+    fat = Column(Float, default=0.0)
+    sugar = Column(Float, default=0.0)
+    salt = Column(Float, default=0.0)
+    vegetarian = Column(Boolean)
+    likes = Column(Integer, default=0)
+    dislikes = Column(Integer, default=0)
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String,index=True, unique=True)
+    hashed_pword = Column(String)
--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
-from endpoints import hello_world
+from endpoints import create_new_recipe, auth_router
+from db.base import engine
+from models import db_models
 
+db_models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(
-    hello_world.router,
+    create_new_recipe.router,
+    responses={404:{"description":"not found"}}
+)
+
+app.include_router(
+    auth_router.router,
     responses={404:{"description":"not found"}}
 )
 
